1a3275b9ea5bd78f349b285f6b4d680806680d62
Shubham Minglani <shubham@linux.com>
2017-12-23 17:48:31 +0530

    minor modifications

--- selection-sort/main.go
+++ selection-sort/main.go
@@ -21,7 +21,6 @@ func main() {
 }
 
 func getSmallestElementIndex(input []int) int {
-	// [65 6 14 7]
 	smallest := input[0]
 	pos := 0
 	for i := 1; i < len(input); i++ {
@@ -38,9 +37,9 @@ func selectionSort(input []int) {
 		log.Fatal("The input array has no elements")
 	}
 	log.Printf("Input array: %v\n\n", input)
-	for pos := 0; pos < len(input); pos++ {
-		// We need to add pos to the returned index since the index is relative to
-		// the sliced array, not the original input array
+	for pos := range input {
+		// We need to add pos to the returned index since the index is relative
+		// to the sliced array, not the original input array
 		smallestPos := getSmallestElementIndex(input[pos:]) + pos
 		log.Printf("Replacing first element %v with the smallest element %v", input[pos], input[smallestPos])
 		input[pos], input[smallestPos] = input[smallestPos], input[pos]
