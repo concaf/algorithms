34625851566d26afec9e50145daa627e39aa4cb0
Shubham Minglani <shubham@linux.com>
2018-02-18 13:20:30 +0530

    clean up inorder implementation

--- trees/inorder-traversal-iterative.go
+++ trees/inorder-traversal-iterative.go
@@ -39,55 +39,26 @@ func inorderTraversal(A *treeNode) []int {
 		}
 	}
 	return arr
-
-	//if r == nil {
-	//	return []int{}
-	//}
-	//
-	//st := make([]*treeNode, 0)
-	//node := r
-	//for node != nil {
-	//	st = append(st, node)
-	//	node = node.left
-	//}
-	//
-	//result := make([]int, 0)
-	//for len(st) > 0 {
-	//	node = st[len(st)-1]
-	//	st = st[:len(st)-1]
-	//
-	//	result = append(result, node.value)
-	//	if node.right != nil {
-	//
-	//		node = node.right
-	//		for node != nil {
-	//			st = append(st, node)
-	//			node = node.left
-	//		}
-	//	}
-	//}
-	//
-	//return result
 }
 
 func main() {
-	//eight := &treeNode{
-	//	value: 8,
-	//}
-	//seven := &treeNode{
-	//	value: 7,
-	//}
-	//six := &treeNode{
-	//	value: 6,
-	//	left:  seven,
-	//	right: eight,
-	//}
+	eight := &treeNode{
+		value: 8,
+	}
+	seven := &treeNode{
+		value: 7,
+	}
+	six := &treeNode{
+		value: 6,
+		left:  seven,
+		right: eight,
+	}
 	five := &treeNode{
 		value: 5,
 	}
 	four := &treeNode{
 		value: 4,
-		//right: six,
+		right: six,
 	}
 	three := &treeNode{
 		value: 3,
