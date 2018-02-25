98b8eedcd89a2e3175f01fd2340fb694ea4d37b3
Shubham Minglani <shubham@linux.com>
2018-02-25 20:40:10 +0530

    add first pass of black shapes

---
+++ graphs/black-shapes-non-optimal.go
@@ -0,0 +1,143 @@
+package main
+
+import "fmt"
+
+/**
+ * @input A : array of strings
+ *
+ * @Output Integer
+ */
+func black(A []string) int {
+	count := 0
+	var visited []X
+	for num, str := range A {
+		for pos, char := range str {
+			if string(char) == "X" {
+				node := X{
+					stringNumber: num,
+					position:     pos,
+				}
+				fmt.Printf("Found X at %v\n", node)
+				if !node.isVisited(visited) {
+					fmt.Printf("Node was not visited before: %v\n", node)
+					fmt.Printf("Starting BFS at: %v\n", node)
+					visited = node.bfs(visited, A)
+					count++
+					fmt.Printf("Total black count is now: %v\n", count)
+				}
+			}
+		}
+	}
+	return count
+}
+
+type X struct {
+	stringNumber int
+	position     int
+}
+
+func (node *X) bfs(visited []X, input []string) []X {
+	var queue []X
+	queue = append(queue, *node)
+
+	for len(queue) > 0 {
+		fmt.Printf("Queue is: %v\n", queue)
+		current := queue[0]
+		fmt.Printf("Current element: %v\n", current)
+		visited = current.markVisited(visited)
+		queue = queue[1:]
+		fmt.Printf("Visited nodes are: %v\n", visited)
+
+		queue = append(queue, current.getNeighbors(input, visited)...)
+	}
+	return visited
+}
+
+func (node *X) markVisited(visited []X) []X {
+	return append(visited, *node)
+}
+
+func (node *X) isVisited(visited []X) bool {
+	for _, alreadyVisited := range visited {
+		if *node == alreadyVisited {
+			return true
+		}
+	}
+	return false
+}
+
+func (node *X) isX(input []string) bool {
+	if string(input[node.stringNumber][node.position]) == "X" {
+		return true
+	}
+	return false
+}
+
+func (node *X) getNeighbors(input []string, visited []X) []X {
+	var neighbors []X
+
+	// Add left
+	if node.position > 0 {
+		potentialNeighbor := X{
+			stringNumber: node.stringNumber,
+			position:     node.position - 1,
+		}
+		if potentialNeighbor.isX(input) && !potentialNeighbor.isVisited(visited) {
+			neighbors = append(neighbors, potentialNeighbor)
+		}
+	}
+
+	// Add right
+	if node.position < len(input[node.stringNumber])-1 {
+		potentialNeighbor := X{
+			stringNumber: node.stringNumber,
+			position:     node.position + 1,
+		}
+		if potentialNeighbor.isX(input) && !potentialNeighbor.isVisited(visited) {
+			neighbors = append(neighbors, potentialNeighbor)
+		}
+	}
+
+	// Add top
+	if node.stringNumber > 0 && string(input[node.stringNumber-1][node.position]) == "X" {
+		potentialNeighbor := X{
+			stringNumber: node.stringNumber - 1,
+			position:     node.position,
+		}
+		if potentialNeighbor.isX(input) && !potentialNeighbor.isVisited(visited) {
+			neighbors = append(neighbors, potentialNeighbor)
+		}
+	}
+
+	// Add bottom
+	if node.stringNumber < len(input)-1 {
+		potentialNeighbor := X{
+			stringNumber: node.stringNumber + 1,
+			position:     node.position,
+		}
+		if potentialNeighbor.isX(input) && !potentialNeighbor.isVisited(visited) {
+			neighbors = append(neighbors, potentialNeighbor)
+		}
+	}
+
+	fmt.Printf("%v has neighbors: %v\n", *node, neighbors)
+	return neighbors
+}
+
+func main() {
+	input := []string{
+		"OOOXOOO",
+		"OOXXOXO",
+		"OXOOOXO",
+		"OOXXXOO",
+		"OOXXXOO",
+		"OOXXXOO",
+	}
+	fmt.Sprintf("Input is:\n")
+	for _, str := range input {
+		fmt.Println(str)
+	}
+
+	count := black(input)
+	fmt.Printf("There are %v black shapes\n", count)
+}
