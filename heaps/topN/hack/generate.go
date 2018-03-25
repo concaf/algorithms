641c9ef11c485a221c94aac812438963a7e99a51
Shubham Minglani <shubham@linux.com>
2018-03-25 21:36:10 +0530

    add file generator code

--- heaps/topN/hack/generate.go
+++ heaps/topN/hack/generate.go
@@ -1,1 +1,30 @@
-package hack
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"os"
+)
+
+func main() {
+    generateAndWrite(100000000, "input.txt")
+}
+
+func generateAndWrite(n int, filename string) {
+	f, err := os.Create(filename)
+	checkError(err)
+	defer f.Close()
+
+	for i := 0; i < n; i++ {
+		data := rand.Int()
+		_, err := f.WriteString(fmt.Sprintf("%d\n", data))
+		checkError(err)
+	}
+}
+
+func checkError(err error) {
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
