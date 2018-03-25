ba5b85a7b84270e34829a10a53c08a4080304f5c
Shubham Minglani <shubham@linux.com>
2018-03-25 22:04:52 +0530

    implement reading the generated file

--- heaps/topN/main.go
+++ heaps/topN/main.go
@@ -1,14 +1,43 @@
+/*
+Write a program, topN, that given an arbitrarily large file and a number, N, containing individual numbers on each line (e.g. 200Gb file), will output the largest N numbers, highest first.
+*/
+
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"github.com/containscafeine/algorithms/heaps/topN/algorithm"
+	"os"
+	"strconv"
 )
 
-func main() {
-	heap := algorithm.New()
-	heap.Insert(100)
+const fileName = "input.txt"
 
-	heap.ExtractMin()
+func main() {
+
+	f, err := os.Open(fileName)
+	checkError(err)
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+
+	heap := algorithm.New()
+	var count int
+	for scanner.Scan() {
+		count++
+		fmt.Println(count)
+		input, err := strconv.Atoi(scanner.Text())
+		checkError(err)
+		heap.Insert(input)
+	}
 	fmt.Println(heap.GetMin())
+
+}
+
+func checkError(err error) {
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
